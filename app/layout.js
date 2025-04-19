c0c40aeb46a0e4a4f3ea6b89ebad20cec2a1fb14
sravankumar <mekalasravankumar66@gmail.com>
2025-04-19 08:13:19 +0530

    second commit

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar /> {/* Include the Navbar here */}
+        <Navbar /> {/* Include the Navbar here   */}
         <main>{children}</main> {/* Your page content */}
       </body>
     </html>
